7fe473fab94dafcf727dfdb48fc0619940b3c9dc
ISNICEI <49389754+ISNICEI@users.noreply.github.com>
2025-04-03 16:01:04 +0300

    Try

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -6,18 +6,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-
-    @Qualifier("dogBean")
-    @Autowired
     private Animal animal;
-    @Autowired
     private Timer timer;
+
+    @Autowired
+    public AnimalsCage(@Qualifier("dogBean") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(timer.getTime());
+        System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
 
